f6c6bd42c271864e67f87b93c42224c648b8662f
Randy Blea <blead@us.ibm.com>
2022-05-03 16:50:01 -0500

    hardware_client doc changes

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -28,7 +29,7 @@ author = 'dblea00'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Add any Sphinx extension module names here, as strings
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Napoleon settings
 napoleon_google_docstring = True

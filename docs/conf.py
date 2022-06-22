9e5c10d023d0442b33aee4536c29604b3ee75fa5
Randy Blea <blead@us.ibm.com>
2022-06-22 14:08:16 -0500

    Additional changes

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -24,12 +24,17 @@ author = 'dblea00'
 
 
 # -- General configuration ---------------------------------------------------
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Add any Sphinx extension module names here, as strings
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+if on_rtd:
+    extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+else:
+    extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -61,7 +66,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+if on_rtd:
+    html_theme = 'sphinx_rtd_theme'
+else:
+    html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

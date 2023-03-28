e05f44bd7da7c45c88ab6ca33430169f6bd4a67d
Randy Blea <92941033+blearandy@users.noreply.github.com>
2023-03-28 14:53:00 -0500

    Update version number for  1.0.2
    Signed-off-by: Randy Blea <blead@us.ibm.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="CSM Python Client",
     long_description="CSM RESTful API Python Client.",
     author="Dominic Blea",
-    version='1.0.1',
+    version='1.0.2',
     author_email="dblea00@ibm.com",
     maintainer="Dominic Blea",
     keywords=["IBM", "CSM Storage"],
